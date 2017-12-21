dec24ca1e610ea37db06356827dd7af36210b895
viminaria <kirzickah@gmail.com>
2017-12-21 10:17:34 +0200

    fixed image scaling a bit

--- src/slide.js
+++ src/slide.js
@@ -1,28 +1,31 @@
 var x = 1;
 var changeslide = document.getElementById('slide');
-var bildtext = [
+var pictext = [
 ['<p id="picturetext">Googly Eyes Necromancer</p>'],
-['<p id="picturetext">Redout 4K</p>']
+['<p id="picturetext">Redout 4K</p>'],
+['<p id="picturetext">Placeholder 3</p>'],
+['<p id="picturetext">Placeholder 4</p>'],
+['<p id="picturetext">Placeholder 5</p>'],
 ];
-
+	
 function prevImg() {
 	if (x > 1) {
 		x--;
-		changeslide.innerHTML  = '<img src="img/' + x + '.png" id="slideimg">' + bildtext[x-1];
+		changeslide.innerHTML  = '<img src="img/' + x + '.png" id="slideimg">' + pictext[x-1] + '<p id="picturetext">' + x + '/5</p>';
 	}
 	else if (x = 1) {
-		x = 2;
-		changeslide.innerHTML  = '<img src="img/' + x + '.png" id="slideimg">' + bildtext[x-1];
+		x = 5;
+		changeslide.innerHTML  = '<img src="img/' + x + '.png" id="slideimg">' + pictext[x-1] + '<p id="picturetext">' + x + '/5</p>';
 	}
 }
 
 function nextImg() {
-	if (x < 2) {
+	if (x < 5) {
 		x++;
-		changeslide.innerHTML  = '<img src="img/' + x + '.png" id="slideimg">' + bildtext[x-1];
+		changeslide.innerHTML  = '<img src="img/' + x + '.png" id="slideimg">' + pictext[x-1] + '<p id="picturetext">' + x + '/5</p>';
 	}
-	else if (x = 2) {
+	else if (x = 5) {
 		x = 1;
-		changeslide.innerHTML  = '<img src="img/' + x + '.png" id="slideimg">' + bildtext[x-1];
+		changeslide.innerHTML  = '<img src="img/' + x + '.png" id="slideimg">' + pictext[x-1] + '<p id="picturetext">' + x + '/5</p>';
 	}
 }
