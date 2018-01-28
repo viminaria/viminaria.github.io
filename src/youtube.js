67ec6655b7be0f5bb8006e7f2a5f8fd2de064fb6
viminaria <kirzickah@gmail.com>
2018-01-29 01:24:50 +0200

    added preload images to parallax videos

---
+++ src/youtube.js
@@ -0,0 +1,54 @@
+var player;
+var player2;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('ytplayer', {
+        videoId: '-guAClQdOq4',
+        playerVars: {
+            mute: 1,
+            autoplay: 1,
+            loop: 1,
+            controls: 0,
+            showinfo: 0,
+            autohide: 0,
+            enablejsapi: 1,
+            modestbranding: 1,
+            playlist: '-guAClQdOq4',
+            vq: 'hd1080'
+        },
+        allowfullscreen: 1,
+        events: {
+            'onStateChange': onPlayerStateChange
+        }
+    });
+
+    player2 = new YT.Player('ytplayer2', {
+        videoId: 'eNF7jo9P_EQ',
+        playerVars: {
+            mute: 1,
+            autoplay: 1,
+            loop: 1,
+            controls: 0,
+            showinfo: 0,
+            autohide: 0,
+            enablejsapi: 1,
+            modestbranding: 1,
+            playlist: 'eNF7jo9P_EQ',
+            vq: 'hd1080'
+        },
+        allowfullscreen: 1,
+        events: {
+            'onStateChange': onPlayer2StateChange
+        }
+    });
+}
+function onPlayerStateChange(el) {
+    if(el.data === 1) {
+        jQuery('#video-overlay').hide();
+    }
+}
+function onPlayer2StateChange(el) {
+    if(el.data === 1) {
+        jQuery('#video-overlay2').hide();
+    }
+}
