795907b3f32ecf884b159936ddc3383a2b2ddee7
viminaria <kirzickah@gmail.com>
2018-02-17 19:49:15 +0200

    changed opacity of particles and lines on donate

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ particlesJS('particles-js',
       }
     },
     "opacity": {
-      "value": 0.5,
+      "value": 0.3,
       "random": false,
       "anim": {
         "enable": false,
@@ -51,7 +51,7 @@ particlesJS('particles-js',
       "enable": true,
       "distance": 150,
       "color": "#ffffff",
-      "opacity": 0.4,
+      "opacity": 0.2,
       "width": 1
     },
     "move": {
