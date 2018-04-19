52b36bf52c06e39bd09e9a55e6adfe1101bc73af
viminaria <kirzickah@gmail.com>
2018-04-19 14:00:03 +0300

    added transit banner ad

--- test/src/test.js
+++ test/src/test.js
@@ -5,5 +5,16 @@ $('.transitbutton').click(function(){
     .transition({scale:'0.2'}, 300)
     .transition({scale:'1'}, 300);
     $('#transitobject2').transition({x:'-30px', delay: 1200}, 600)
-    .transition({x:'0px'}, 600);
+    .transition({opacity: '0.5'}, 600);
+});
+
+$(document).ready(function eyes(){
+    $('#transit-item1').transition({opacity: '1', delay: 1000}, 1000)
+    .transition({opacity: '0', delay: 5000}, 1000);
+
+    $('#transit-text1').transition({opacity: '1', delay: 2000}, 1000)
+    .transition({opacity: '0', delay: 1000}, 1000)
+    .transition({opacity: '0'}, 3000);
+
+    eyes();
 });
