e24bed396916875cd0fd66281530cbe0f3df2101
viminaria <kirzickah@gmail.com>
2018-04-23 11:33:11 +0300

    iterated transitad

--- test/src/test.js
+++ test/src/test.js
@@ -1,10 +1,15 @@
 $(document).ready(function transitAd(){
-    $('#transitad-item1').transition({opacity: '1', delay: 1000}, 1000)
-    .transition({opacity: '0', delay: 5000}, 1000);
+    $('#transitad-item1').transition({opacity: 1}, 1000)
+    .transition({opacity: 0, delay: 9000}, 1000)
+    .transition({opacity: 0, delay: 5000}, 1000);
+        
 
-    $('#transitad-text1').transition({opacity: '1', delay: 2000}, 1000)
-    .transition({opacity: '0', delay: 1000}, 1000)
-    .transition({opacity: '0'}, 3000);
+    $('#transitad-item2').transition({opacity: 1, delay: 11000}, 1000)
+    .transition({opacity: 0, delay: 4000}, 1000);
+
+    $('#transitad-text').transition({opacity: 1, delay: 1000}, 1500)
+    .transition({opacity: 0, delay: 2000}, 1500)
+    .transition({opacity: 0}, 11000);
 
     transitAd();
 });
