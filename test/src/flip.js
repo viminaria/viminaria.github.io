ac2ec9438f26f9ae878d5b32f8adde2511808296
viminaria <kirzickah@gmail.com>
2018-04-26 19:43:50 +0300

    added music to starfield

--- test/src/flip.js
+++ test/src/flip.js
@@ -1,10 +1,10 @@
-$('#stage').mouseenter(function(){
-    $('#flip-animation-box').transition({rotateY: '+=180deg'}, 800);
+$('#stage').mouseenter(function flip(){
+    $('#flip-animation-box').transition({rotateY: '+=180deg'}, 850);
     $('#flip-item-1').transition({borderRadius: '40vmin'}, 800);
     $('#flip-item-2').transition({borderRadius: '40vmin'}, 800);
 });
 
-$('#stage').mouseleave(function(){
+$('#stage').mouseleave(function flipBack(){
     $('#flip-animation-box').transition({rotateY: '+=180deg'}, 800);
     $('#flip-item-1').transition({borderRadius: '0vmin'}, 800);
     $('#flip-item-2').transition({borderRadius: '0vmin'}, 800);
