fcd85e325094533388a2b391c6ec267602d09982
viminaria <kirzickah@gmail.com>
2018-04-26 22:56:20 +0300

    added glow to starfield

--- test/src/starfield.js
+++ test/src/starfield.js
@@ -1,28 +1,29 @@
-var starcount = 150;
+var starcount = 400;
 var styletag = document.createElement("style");
 document.head.appendChild(styletag);
 var starcss = styletag.sheet;
 
 for (i=1; i<=starcount; i++){
     $('#starfield').append('<div class="starfield-star"></div>');
-    var starsize = 0.5 + Math.random()*5;
-    var animdelay = Math.random()*-10;
-    if (starsize > 5) {
-        starcolor = "#7777ff";
+    var starsize = 1 + Math.random()*2;
+    var animdelay = Math.random()*-50;
+
+    if (starsize > 2.7){
+        starcolor = "skyblue";
     }
-    else if (starsize > 4){
-        starcolor = "lightblue";
-    }
-    else if (starsize > 2.5){
+    else if (starsize > 2.4){
         starcolor = "orange";
     }
-    else if (starsize < 1.5){
+    else if (starsize > 1.8){
+        starcolor = "hotred";
+    }
+    else if (starsize > 1.5){
         starcolor = "red";
     }
     else {
-        starcolor = "brown";
+        starcolor = "white";
     }
 
     var trans3d = Math.floor(Math.random()*100) + "vw, " + Math.floor(Math.random()*80) + "vh, " + Math.floor(Math.random()*1000) + "vmin";
-    starcss.insertRule('.starfield-star:nth-child('+ i +'){width: '+ starsize +'vmin; height: '+ starsize +'vmin;transform: translate3d('+ trans3d +');background-color:'+ starcolor +';animation-delay:'+ animdelay +'s;}', 0);
+    starcss.insertRule('.starfield-star:nth-child('+ i +'){width: '+ starsize +'vmin; height: '+ starsize +'vmin;transform: translate3d('+ trans3d +');background-color:'+ starcolor +';box-shadow: 0px 0px 3vmin 0.2vmin '+ starcolor +';animation-delay:'+ animdelay +'s;}', 0);
 }
