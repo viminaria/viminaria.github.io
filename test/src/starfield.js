d906fc4d3c1d216b1fbdf991cbd452f84f9c417c
viminaria <kirzickah@gmail.com>
2018-05-03 09:25:21 +0300

    fixed star glow

--- test/src/starfield.js
+++ test/src/starfield.js
@@ -27,5 +27,5 @@ for (i=1; i<=starcount; i++){
     }
 
     var trans3d = Math.floor(Math.random()*400-starsize*25) + "vw, " + Math.floor(Math.random()*400-starsize*25) + "vh, " + Math.floor(Math.random()*100) + "vmin";
-    starcss.insertRule('.starfield-star:nth-child('+ i +'){width: '+ starsize +'vmin; height: '+ starsize +'vmin;transform: translate3d('+ trans3d +');background-color:'+ starcolor +';box-shadow: 0px 0px 12vmin '+ starsize +'vmin '+ starcolor +';animation-delay:'+ animdelay +'s;animation-duration:' + animdur + 's;}', 0);
+    starcss.insertRule('.starfield-star:nth-child('+ i +'){width: '+ starsize +'vmin; height: '+ starsize +'vmin;transform: translate3d('+ trans3d +');background-color:'+ starcolor +';box-shadow: 0px 0px 12vmin '+ starsize/3 +'vmin '+ starcolor +';animation-delay:'+ animdelay +'s;animation-duration:' + animdur + 's;}', 0);
 }
