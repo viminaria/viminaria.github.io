ad907aec8cf9682761156ca59dc786e51b5fdf9d
viminaria <kirzickah@gmail.com>
2018-05-08 23:20:02 +0300

    added flyaway button

--- test/src/starfield.js
+++ test/src/starfield.js
@@ -1,7 +1,17 @@
+var audio = document.getElementById('starfield-audio');
+audio.src = "src/starfield.ogg"
+$('audio').prop("volume", 0.1);
+audio.play();
+
 var starcount = 300;
+var particlecount = 30;
 var styletag = document.createElement("style");
+var particlestyletag = document.createElement("style");
 document.head.appendChild(styletag);
+document.head.appendChild(particlestyletag);
 var starcss = styletag.sheet;
+var particlecss = particlestyletag.sheet;
+
 
 for (i=1; i<=starcount; i++){
     $('#starfield').append('<div class="starfield-star"></div>');
@@ -10,22 +20,30 @@ for (i=1; i<=starcount; i++){
     var animdelay = Math.floor(Math.random()*-400);
     var animdur = 40 - starsize*3;
 
-    if (starcolorrand > 10){
-        starcolor = "rgb(210, 210, 255)";
-    }
-    else if (starcolorrand > 7){
-        starcolor = "rgb(255, 255, 220)";
-    }
-    else if (starcolorrand > 5){
-        starcolor = "rgb(255, 255, 180)";
-    }
-    else if (starcolorrand > 4){
-        starcolor = "rgb(255, 180, 180)";
-    }
-    else {
-        starcolor = "white";
-    }
+    if (starcolorrand > 10){ starcolor = "rgb(210, 210, 255)"; }
+    else if (starcolorrand > 7){ starcolor = "rgb(255, 255, 220)"; }
+    else if (starcolorrand > 5){ starcolor = "rgb(255, 255, 180)"; }
+    else if (starcolorrand > 4){ starcolor = "rgb(255, 180, 180)"; }
+    else { starcolor = "white"; }
 
     var trans3d = Math.floor(Math.random()*400-starcolorrand*25) + "vw, " + Math.floor(Math.random()*400-starcolorrand*25) + "vh, " + Math.floor(Math.random()*100) + "vmin";
     starcss.insertRule('.starfield-star:nth-child('+ i +'){width: '+ starsize +'vmin; height: '+ starsize +'vmin;transform: translate3d('+ trans3d +');background-color:'+ starcolor +';box-shadow: 0px 0px 12vmin '+ starsize/3 +'vmin '+ starcolor +';animation-delay:'+ animdelay +'s;animation-duration:' + animdur + 's;}', 0);
+}
+
+for (i=1; i<=particlecount; i++){
+    $('#starfield-flyer').append('<div class="starfield-flyer-particle"></div>');
+    var particlesize = 1 + Math.random()*1.5;
+    var animdelay = Math.random()*-10;
+
+    var trans3d = Math.random()*12 + "vmin, " + "0, " + " 0";
+    particlecss.insertRule('.starfield-flyer-particle:nth-child('+ i +'){width: '+ particlesize +'vmin; height: '+ particlesize +'vmin;transform: translate3d('+ trans3d +');animation-delay:'+ animdelay +'s;background-color: yellow;}', 0);
+}
+
+
+$('.flyaway-button').transition({scale: 1, opacity: 1, delay: 5000}, 500);
+
+function flyAway(){
+    $('#starfield-flyer').css("animation", "forwards");
+    $('#starfield-flyer').transition({y:'-18vh', scale: 0.001, rotateX: '50deg'}, 5000);
+    $('.flyaway-button').transition({opacity: 0}, 500);
 }
