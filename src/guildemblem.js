02b7cc1aa79c80cf832e6bbed7b742526e3c8208
viminaria <kirzickah@gmail.com>
2020-06-07 20:25:27 +0300

    added downscaler

--- src/guildemblem.js
+++ src/guildemblem.js
@@ -1,3 +1,4 @@
+/*
 var fileTag = document.getElementById("guildemblem_upload"),
     preview = document.getElementById("previewHolder");
     
@@ -16,4 +17,51 @@ function changeImage(input) {
 
     reader.readAsDataURL(input.files[0]);
   }
+}
+*/
+
+var fileReader = new FileReader();
+var filterType = /^(?:image\/bmp|image\/cis\-cod|image\/gif|image\/ief|image\/jpeg|image\/jpeg|image\/jpeg|image\/pipeg|image\/png|image\/svg\+xml|image\/tiff|image\/x\-cmu\-raster|image\/x\-cmx|image\/x\-icon|image\/x\-portable\-anymap|image\/x\-portable\-bitmap|image\/x\-portable\-graymap|image\/x\-portable\-pixmap|image\/x\-rgb|image\/x\-xbitmap|image\/x\-xpixmap|image\/x\-xwindowdump)$/i;
+
+fileReader.onload = function (event) {
+  var image = new Image();
+  
+  image.onload = function(){
+      document.getElementById("original-Img").src=image.src;
+      var canvas=document.createElement("canvas");
+      var context=canvas.getContext("2d");
+      canvas.width=17;
+      canvas.height=17;
+      context.drawImage(image,
+          0,
+          0,
+          image.width,
+          image.height,
+          0,
+          0,
+          canvas.width,
+          canvas.height
+      );
+      
+      document.getElementById("upload-Preview").src = canvas.toDataURL();
+  }
+  image.src=event.target.result;
+};
+
+var loadImageFile = function () {
+  var uploadImage = document.getElementById("upload-Image");
+  
+  //check and retuns the length of uploded file.
+  if (uploadImage.files.length === 0) { 
+    return; 
+  }
+  
+  //Is Used for validate a valid file.
+  var uploadFile = document.getElementById("upload-Image").files[0];
+  if (!filterType.test(uploadFile.type)) {
+    alert("Please select a valid image."); 
+    return;
+  }
+  
+  fileReader.readAsDataURL(uploadFile);
 }
